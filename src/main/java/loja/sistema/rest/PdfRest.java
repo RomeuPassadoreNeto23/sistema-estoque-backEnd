35a327f034974dab90a0f54930e8369e7050054d
Romeu <passadore.neto@outlook.com>
2023-02-18 17:13:20 -0300

    add PdfRest

---
+++ src/main/java/loja/sistema/rest/PdfRest.java
@@ -0,0 +1,79 @@
+package loja.sistema.rest;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.io.Files;
+
+import loja.sistema.model.Pedido;
+import loja.sistema.repository.PedidoRepository;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
+@RestController
+@RequestMapping("api/pdf")
+@CrossOrigin
+public class PdfRest {
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@GetMapping(value = "/pedido/{id}")
+	public String pdfPedidoById(@PathVariable("id") Long id, HttpServletRequest request, HttpServletResponse response) {
+		Pedido pedido = new Pedido();
+		pedido = pedidoRepository.findById(id).get();
+		try {
+			JasperReport report = JasperCompileManager
+					.compileReport(getClass().getResourceAsStream("/relatorio/PdfPedido.jrxml"));
+			Map<String, Object> map = new HashMap<>();
+			map.put("nomeCompleto", pedido.getNomeCompleto());
+			map.put("quantidadeDeProduct", pedido.getQuantidadeDeProduct());
+			map.put("descricaoDoProduct", pedido.getProduct().getDescricaoDoProduct());
+			map.put("precoDoProduct", pedido.getProduct().getPrecoDoProduct());
+			map.put("precoTotal", pedido.getPrecoTotal());
+
+			String name = "PEDIDO.pdf";
+
+			JasperPrint print = JasperFillManager.fillReport(report, map, new JREmptyDataSource());
+
+			JasperExportManager.exportReportToPdfFile(print, name);
+
+			File arquivo = new File(name);
+
+			OutputStream output = response.getOutputStream();
+
+			Files.copy(arquivo, output);
+
+			output.close();
+
+		} catch (JRException e) {
+			
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+		return "ok";
+
+	}
+
+}
